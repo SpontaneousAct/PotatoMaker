5d4eab0f04eb64b1fe86ef3b7e2473ef62004d23
SpontaneousAct <15077832+SpontaneousAct@users.noreply.github.com>
2026-03-02 12:52:59 +0100

    Add UI project.

---
+++ PotatoMaker.GUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PotatoMaker.GUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
